2ad275f04de48c0897d19016d0120cc428aec534
Rui Miguel <ravage@fragmentized.net>
2009-04-02 21:51:26 +0100

    Added down method for migrations

--- migrations/001_database.rb
+++ migrations/001_database.rb
@@ -2,117 +2,157 @@ class CreateTables < Sequel::Migration
   def up
     create_table(:agenda) do
       primary_key :id
-      String :description
+      
+      String      :description
     end
     
     create_table(:blocks) do
       primary_key :id
-      varchar :title, :size => 80
-      longtext :content
-      foreign_key :parent, :blocks, :key => :id
-      index :parent
+     
+      varchar     :title, :size => 80
+      longtext    :content
+      
+      foreign_key :parent, :table => :blocks, :key => :id
+      index       :parent
     end
     
     create_table(:categories) do
       primary_key :id
-      varchar :description, :size => 20
+      
+      varchar     :description, :size => 20
     end
     
     create_table(:comments) do
       primary_key :id
-      longtext :content
-      DateTime :created_at, :default => :CURRENT_TIMESTAMP
-      foreign_key :parent, :comments, :key => :id
-      foreign_key :user_id, :users, :key => :id
-      index :parent
-      index :user_id
+      
+      longtext    :content
+      DateTime    :created_at, :default => :CURRENT_TIMESTAMP
+      
+      foreign_key :parent, :table => :comments, :key => :id
+      foreign_key :user_id, :table => :users, :key => :id
+      index       :parent
+      index       :user_id
     end
     
     create_table(:albums) do
       primary_key :id
-      varchar :title, :size => 60
-      foreign_key :user_id, :users, :key => :id
-      foreign_key :category_id, :categories, :key => :id
+      
+      varchar     :title, :size => 60
+      
+      foreign_key :user_id, :table => :users, :key => :id
+      foreign_key :category_id, :table => :categories, :key => :id
     end
     
     create_table(:band_elements) do
-      foreign_key :user_id, :users, :key => :id
-      foreign_key :element_id, :users, :key => :id
+      foreign_key :user_id, :table => :users, :key => :id
+      foreign_key :element_id, :table => :users, :key => :id
     end
     
     create_table(:events) do
       primary_key :id
-      foreign_key :map_point_id, :map_points, :key => :id
-      foreign_key :agenda_id, :agenda, :key => :id
-      varchar :name, :size => 80
-      longtext :description
+      
+      varchar     :name, :size => 80
+      longtext    :description
+        
+      foreign_key :map_point_id, :table => :map_points, :key => :id
+      foreign_key :agenda_id, :table => :agenda, :key => :id
     end
     
     create_table(:map_points) do
       primary_key :id
-      Float :latitude
-      Float :longitude
-      varchar :name, :size => 80
-      longtext :description
+     
+      Float     :latitude
+      Float     :longitude
+      varchar   :name, :size => 80
+      longtext  :description
     end
     
     create_table(:mailing_list_messages) do
       primary_key :id
-      DateTime :created_at, :default => :CURRENT_TIMESTAMP
-      foreign_key :mailing_list_id, :mailing_list, :key => :id
-      longtext :content
+      
+      DateTime    :created_at, :default => :CURRENT_TIMESTAMP
+      longtext    :content
+      
+      foreign_key :mailing_list_id, :table => :mailing_list, :key => :id
     end
     
     create_table(:mailing_list) do
       primary_key :id
-      TrueClass :state
+      
+      TrueClass   :state
     end
     
     create_table(:tracks) do
       primary_key :id
-      foreign_key :album_id, :albums, :key => :id
-      varchar :title, :size => 60
-      smallint :number
-      text :lyrics
-      varchar :path, :size => 60
+      
+      varchar     :title, :size => 60
+      smallint    :number
+      text        :lyrics
+      varchar     :path, :size => 60
+      
+      foreign_key :album_id, :table => :albums, :key => :id
     end
     
     create_table(:countries) do
       primary_key :id
-      varchar :abbr, :size => 3
-      varchar :country, :size => 30
+    
+      varchar     :abbr, :size => 3
+      varchar     :country, :size => 30
     end
     
     create_table(:profiles) do
       primary_key :id
-      foreign_key :country_id, :countries, :key => :id
-      foreign_key :map_point_id, :map_points, :key => :id
-      String :address
-      varchar :province, :size => 30
-      varchar :zip_code, :size => 10
-      varchar :city, :size => 30
-      String :real_name
-      varchar :photo_path, :size => 60
-      text :preferences
+     
+      String      :address
+      varchar     :province, :size => 30
+      varchar     :zip_code, :size => 10
+      varchar     :city, :size => 30
+      String      :real_name
+      varchar     :photo_path, :size => 60
+      text        :preferences
+      
+      foreign_key :country_id, :table => :countries, :key => :id
+      foreign_key :map_point_id, :table => :map_points, :key => :id
     end
     
     create_table(:users) do
       primary_key :id
-      foreign_key :mailing_list_id, :mailing_lists, :key => :id
-      foreign_key :profile_id, :profiles, :key => :id
-      varchar :email, :size => 100, :unique => true
-      varchar :password, :size => 128
-      foreign_key :agenda_id, :agenda, :key => :id
+  
+      varchar     :email, :size => 100, :unique => true
+      varchar     :password, :size => 128
+      
+      foreign_key :agenda_id, :table => :agenda, :key => :id
+      foreign_key :mailing_list_id, :table => :mailing_lists, :key => :id
+      foreign_key :profile_id, :table => :profiles, :key => :id
     end
     
     create_table(:users_favs) do
-      foreign_key :band_id, :users, :key => :id
-      foreign_key :user_id, :users, :key => :id
+      foreign_key :band_id, :table => :users, :key => :id
+      foreign_key :user_id, :table => :users, :key => :id
     end
     
     create_table(:users_mailing_lists) do
-      foreign_key :mailing_list_id, :mailing_lists, :key => :id
-      foreign_key :user_id, :users, :key => :id
+      foreign_key :mailing_list_id, :table => :mailing_lists, :key => :id
+      foreign_key :user_id, :table => :users, :key => :id
     end
+  end
+  
+  def down
+    drop_table :agenda
+    drop_table :blocks
+    drop_table :categories
+    drop_table :comments
+    drop_table :albums
+    drop_table :band_elements
+    drop_table :events
+    drop_table :map_points
+    drop_table :mailing_list_messages
+    drop_table :mailing_lists
+    drop_table :tracks
+    drop_table :countries
+    drop_table :profiles
+    drop_table :users
+    drop_table :users_favs
+    drop_table :users_mailing_lists
   end
 end
