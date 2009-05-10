32b966470978d27032df4d00e1f20b86fd6d13c9
Rui Miguel <ravage@fragmentized.net>
2009-05-10 20:09:02 +0100

    * Correct the spin script

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,3 +3,4 @@ set :repository, 'ssh://fragmentized_git@fragmentized.net/home/fragmentized_git/
 set :scm, :git
 set :deploy_to, '/home/ravage/ruby_apps'
 server 'soundtape.net', :app, :web
+set :use_sudo, false
