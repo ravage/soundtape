dbd0595a06d9c34f4b4cca3c0b2fd9848a3a4402
Rui Miguel <ravage@fragmentized.net>
2009-05-10 20:49:18 +0100

    * Added config.ru

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,28 @@
 set :application, 'soundtape'
 set :repository, 'ssh://fragmentized_git@fragmentized.net/home/fragmentized_git/git/soundtape.git'
 set :scm, :git
-set :deploy_to, '/home/ravage/ruby_apps'
+set :deploy_to, "/home/ravage/ruby_apps/#{application}"
+set :deploy_via, :remote_cache
 server 'soundtape.net', :app, :web
 set :use_sudo, false
+
+after 'deploy', 'options'
+
+task :options do
+  system("scp options.rb ravage@soundtape.net:/#{deploy_to}/current/.")
+end
+
+namespace :deploy do
+  task :start, :roles => [:web, :app] do
+    run "thin -C /etc/thin/soundtape.yml start" 
+  end
+  
+  task :stop, :roles => [:web, :app] do
+    run "thin -C /etc/thin/soundtape.yml stop" 
+  end
+  
+  task :restart, :roles => [:web, :app] do
+     deploy.stop
+     deploy.start
+   end
+end
