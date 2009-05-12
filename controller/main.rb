11af71893c9444ba182e96f8ba06cac26cdf5527
Rui Miguel <ravage@fragmentized.net>
2009-05-12 02:40:13 +0100

    * Added feedback form * Added some new graphics

--- controller/main.rb
+++ controller/main.rb
@@ -10,4 +10,22 @@ class MainController < Controller
     redirect :/ unless flash[:exception]
     _("An unexpected error ocurred please try again")
   end
+  
+  def feedback
+    @title = _('Feedback')
+    @user = user
+    @profile = user.profile
+    if request.post?
+      msg = %|#{request[:name]} - #{request[:email]} says:
+      #{request[:feedback]}|
+      Ramaze.defer do
+        begin
+          Ramaze::EmailHelper.send(request[:email], '[SoundTape]: Feedback', msg)
+        rescue Exception => e
+          oops(r(:feedback), e)
+        end
+      end
+      flash[:success] = _('Thank you for taking the time to give us some feedback!')
+    end
+  end
 end
