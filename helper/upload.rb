ed3b3066ea8405709d231a552bbec74b920296d3
Rui Miguel <ravage@fragmentized.net>
2009-06-22 17:22:20 +0100

    * Chmod file after copy

--- helper/upload.rb
+++ helper/upload.rb
@@ -58,6 +58,7 @@ module SoundTape
       def move_to(path)
         begin
           FileUtils.cp(tempfile.path, path)
+          FileUtils.chmod(644, path)
         rescue Exception => e 
           raise UploadException, e
         ensure
